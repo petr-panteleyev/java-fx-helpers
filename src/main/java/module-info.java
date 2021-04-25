f24bba943cadd633147b1fb92691b9839b376fd5
Petr Panteleyev <petr@panteleyev.org>
2021-04-26 02:22:12 +0300

    JavaFX Helpers 1.8.1
    * javafx-swing dependency moved to test scope

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,6 @@ module org.panteleyev.fx {
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
 
-    requires javafx.swing;
-
     exports org.panteleyev.fx;
     exports org.panteleyev.fx.combobox;
     exports org.panteleyev.fx.grid;
