21038ca2f131e5b974776386549f928dc26b3cee
Petr Panteleyev <petr-panteleyev@yandex.ru>
2023-09-30 18:24:44 +0300

    JavaFX Helpers 1.13.0
    * JDK 21
    * JavaFX 21
    * TableColumn is not sortable unless comparator is set

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Helper classes and factories for JavaFX.
 
 ## Build
 
-Make sure ```JAVA_HOME``` points to JDK 17+.
+Make sure ```JAVA_HOME``` points to JDK 21+.
 
 Execute the following:
 
--- src/main/java/org/panteleyev/fx/TableColumnBuilder.java
+++ src/main/java/org/panteleyev/fx/TableColumnBuilder.java
@@ -1,5 +1,5 @@
 /*
- Copyright © 2020-2022 Petr Panteleyev <petr@panteleyev.org>
+ Copyright © 2020-2023 Petr Panteleyev <petr@panteleyev.org>
  SPDX-License-Identifier: BSD-2-Clause
  */
 package org.panteleyev.fx;
@@ -116,6 +116,8 @@ public class TableColumnBuilder<S, T> {
         if (comparator != null) {
             column.setComparator(comparator);
             column.setSortable(true);
+        } else {
+            column.setSortable(false);
         }
 
         if (cellFactory != null) {
